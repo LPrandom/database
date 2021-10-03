071841aa37a4fac54ec718db61428fe338f52654
carminecrafting <bruhhitsniko@gmail.com>
2021-10-03 00:38:02 -1000

    fix again!

--- lua-folder/modswhitelist.lua
+++ lua-folder/modswhitelist.lua
@@ -1,33 +1,33 @@
 local owner = {
-	300141009
+	300141009,
 }
 
 local VIP = {
-	2721170080
+	2721170080,
 }
 
 local premID = {
 	2721170080,
-	300141009
+	300141009,
 }
 
 for i,v in pairs(game.Players:GetChildren()) do
 	if table.find(owner,v.UserId) then
 		if v.Character then
-			v.Character:FindFirstChild("Humaoid").DisplayName = "[👑]"..v.DisplayName
+			v.Character:FindFirstChild("Humanoid").DisplayName = "[👑]"..v.DisplayName
 		end
 	elseif table.find(VIP,v.UserId) then
 		if v.Character then
-			v.Character:FindFirstChild("Humaoid").DisplayName = "[👻]"..v.DisplayName
+			v.Character:FindFirstChild("Humanoid").DisplayName = "[👻]"..v.DisplayName
 		end
 	elseif table.find(premID,v.UserId) then
 		if v.Character then
-			v.Character:FindFirstChild("Humaoid").DisplayName = "[⭐]"..v.DisplayName
+			v.Character:FindFirstChild("Humanoid").DisplayName = "[⭐]"..v.DisplayName
 		end
 	else
 		if v.Character then
 			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChild("Humaoid").DisplayName = "[🔒]"..v.DisplayName
+				v.Character:FindFirstChild("Humanoid").DisplayName = "[🔒]"..v.DisplayName
 			end
 		end
 	end
