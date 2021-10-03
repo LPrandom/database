f6fb057d13282d14a54eca905f0b6e410e67d298
carminecrafting <bruhhitsniko@gmail.com>
2021-10-03 13:41:57 -1000

    update

--- lua-folder/modswhitelist.lua
+++ lua-folder/modswhitelist.lua
@@ -30,7 +30,7 @@ for i,v in pairs(game.Players:GetChildren()) do
 		end
 	else
 		if v.Character then
-			if not v.Character.UpperTorso:FindFirstChild('WaistRigAttachment') then
+			if not v.Character.Humanoid:FindFirstChild('Status') then
 				v.Character:FindFirstChild("Humanoid").DisplayName = "[🔒]"..v.DisplayName
 			end
 		end
