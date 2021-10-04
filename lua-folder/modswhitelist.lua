6ed2e17bb825b5558acfd22d46c6861ab3720e9a
carminecrafting <bruhhitsniko@gmail.com>
2021-10-03 15:14:47 -1000

    update

--- lua-folder/modswhitelist.lua
+++ lua-folder/modswhitelist.lua
@@ -5,11 +5,6 @@ local owner = {
 	300141009,
 }
 
-local VIP = {
-	2721170080,
-	2809416873,
-}
-
 local premID = {
 	2721170080,
 	2809416873,
@@ -20,10 +15,6 @@ for i,v in pairs(game.Players:GetChildren()) do
 	if table.find(owner,v.UserId) then
 		if v.Character then
 			v.Character:FindFirstChild("Humanoid").DisplayName = "[👑]"..v.DisplayName
-		end
-	elseif table.find(VIP,v.UserId) then
-		if v.Character then
-			v.Character:FindFirstChild("Humanoid").DisplayName = "[👻]"..v.DisplayName
 		end
 	elseif table.find(premID,v.UserId) then
 		if v.Character then
