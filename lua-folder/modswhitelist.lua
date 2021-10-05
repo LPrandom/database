e91357d5f98d1f7f75e8596a559ec3d0ca4bf589
random <78721749+LPrandom@users.noreply.github.com>
2021-10-04 17:37:42 -1000

    Update

--- lua-folder/modswhitelist.lua
+++ lua-folder/modswhitelist.lua
@@ -9,6 +9,10 @@ local danny = {
 	63448449,
 }
 
+local dahacker6000 = {
+	1535126103,
+}
+
 local premID = {
 	2721170080,
 	2809416873,
@@ -19,6 +23,7 @@ local premID = {
 	814338063, -- Homo's friend
 	270004839, -- Homo's friend
 	1008303948,
+	1535126103,
 }
 
 for i,v in pairs(game.Players:GetChildren()) do
@@ -29,6 +34,10 @@ for i,v in pairs(game.Players:GetChildren()) do
 	elseif table.find(danny,v.UserId) then
 		if v.Character then
 			v.Character:FindFirstChild("Humanoid").DisplayName = "[🔱]"..v.DisplayName
+		end
+	elseif table.find(dahacker6000,v.UserId) then
+		if v.Character then
+			v.Character:FindFirstChild("Humanoid").DisplayName = "[🍆]"..v.DisplayName
 		end
 	elseif table.find(premID,v.UserId) then
 		if v.Character then
