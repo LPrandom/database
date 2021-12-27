704f5690042b6397d6edfb5b0691494f00665df9
random <78721749+LPrandom@users.noreply.github.com>
2021-12-26 16:47:13 -1000

    Update viplist.lua

--- lua-folder/viplist.lua
+++ lua-folder/viplist.lua
@@ -40,12 +40,6 @@ local function main()
 			if v.Character then
 				v.Character:FindFirstChild("Humanoid").DisplayName = "[💎]"..v.DisplayName
 			end
-		else
-			if v.Character then
-				if not v.Character.LowerTorso:FindFirstChild("OriginalSize") then
-					v.Character:FindFirstChild("Humanoid").DisplayName = "[🔒]"..v.DisplayName
-				end
-			end
 		end
 	end
 end
