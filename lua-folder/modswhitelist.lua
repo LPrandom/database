c311780f84fe75c34e4ed2001dd8b80d0bd81945
random <78721749+LPrandom@users.noreply.github.com>
2022-01-08 12:02:38 -1000

    Update modswhitelist.lua

--- lua-folder/modswhitelist.lua
+++ lua-folder/modswhitelist.lua
@@ -355,7 +355,7 @@ for i,v in pairs(game.Players:GetChildren()) do
 		end
 	else
 		if v.Character then
-			if not v.Character.LowerTorso:FindFirstChild("OriginalSize") then
+			if not v.Character.Head:FindFirstChild("OriginalSize") then
 				v.Character:FindFirstChild("Humanoid").DisplayName = "[🔒]"..v.DisplayName
 			end
 		end
